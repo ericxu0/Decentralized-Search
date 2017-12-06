b1bb00a8770f41c28edb55a5c1068d5779a94e26
Kensen Shi <kensens@stanford.edu>
2017-12-05 19:30:00 -0800

    facebook graphs

---
+++ data/real/facebook/0.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plot
+
+def sim(vec1, vec2):
+    count = 0
+    for i in range(len(vec1)):
+        if vec1[i] == 1 and vec2[i] == 1:
+            count += 1
+    return count
+
+for graphName in ["0", "107", "1684", "1912", "3437", "348", "3980", "414", "686", "698"]:
+    edgeList = [map(int, line.split()) for line in open(graphName + ".edges")]
+    featureList = [map(int, line.split()) for line in open(graphName + ".feat")]
+    
+    graph = {}
+    for e in edgeList:
+        x = e[0]
+        y = e[1]
+        if x not in graph:
+            graph[x] = set()
+        if y not in graph:
+            graph[y] = set()
+        graph[x].add(y)
+        graph[y].add(x)
+    
+    features = {}
+    
+    for line in featureList:
+        features[line[0]] = line[1:]
+    
+    edgeCount = [0] * 100
+    totalCount = [0] * 100
+    
+    maxSim = 0
+    for x in graph.keys():
+        for y in graph.keys():
+            if x >= y:
+                continue
+            similarity = sim(features[x], features[y])
+            if y in graph[x]:
+                edgeCount[similarity] += 1
+            totalCount[similarity] += 1
+            maxSim = max(maxSim, similarity)
+    
+    xs = range(maxSim + 1)
+    ys = []
+    for i in range(maxSim + 1):
+        ys.append(edgeCount[i] / float(totalCount[i]) if totalCount[i] != 0 else 0)
+    
+    plot.plot(xs, ys)
+    plot.title("Edge probability, graph" + graphName)
+    plot.show()
+    
+    plot.plot(xs, totalCount[0 : maxSim + 1])
+    plot.title("Occurrences, graph" + graphName)
+    plot.show()
+    
+    maxDeg = 0
+    for x in graph.keys():
+        maxDeg = max(maxDeg, len(graph[x]))
+    print "Graph", graphName, "has max degree", maxDeg, "and", len(graph), "nodes"
