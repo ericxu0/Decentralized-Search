4b5b2f69d66bc7ebe7a0c321a923035dc787c9a7
Albert Tung <atung3@stanford.edu>
2017-12-07 21:47:29 -0800

    Added correct output file in bash script and added facebook n2v embeddings

--- generateNode2Vec.sh
+++ generateNode2Vec.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -x
 find . -type f -name '*.edges' | while read line; do
+	home="."
+	extension=".emb"
+	output=$home$(echo $line | cut -f 2 -d '.')$extension
 
-	output =$(echo $line | cut -f 2 -d '.')
     ./../node2vec/node2vec -i:$line -o:$output -d:'100'  -v
 done
 
